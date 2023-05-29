c53d91baeb189dcf7d7d9cf808a92b128f0d6fd0
marcocareddu <saldumaccu@tuta.io>
2023-05-29 16:02:50 +0200

    JS - Var linked to DOM

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 console.log('JS OK')
 
+// PHASE 1
+
+// Link to elements
+const nameInput = document.getElementById('name');
+const km_amount = document.getElementById('km');
+const calcButton = document.getElementById('calc-btn');
+const eraseButton = document.getElementById('erase-btn');
+const teen = document.getElementById('under18');
+const adult = document.getElementById('adult');
+const over65 = document.getElementById('over65');
+
