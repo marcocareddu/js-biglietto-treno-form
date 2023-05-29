fe5edc8e653c8dfab0a5ec96b5e5032c48b68f77
marcocareddu <saldumaccu@tuta.io>
2023-05-29 16:23:45 +0200

    JS - Fixed var age declaration

--- js/script.js
+++ js/script.js
@@ -7,9 +7,7 @@ const nameInput = document.getElementById('name');
 const km_amount = document.getElementById('km');
 const calcButton = document.getElementById('calc-btn');
 const eraseButton = document.getElementById('erase-btn');
-const teen = document.getElementById('under18');
-const adult = document.getElementById('adult');
-const over65 = document.getElementById('over65');
+const ageInput = document.getElementById('age-select');
 
 // Declare calc variables
 const under18 = 19;
@@ -18,4 +16,6 @@ let discount;
 const costXkm = 0.21;
 
 console.log(under18, senior, discount, costXkm);
+
+
 
